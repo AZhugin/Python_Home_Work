e811b986dfa00d8aab87bc5cdcc8a61430ecad3a
AZhugin <zakxeiy@gmail.com>
2026-02-04 22:52:12 +0100

    Python_HW

---
+++ Alex_Zhugin_121225_ptm/AlexZhugin_HW_2.py
@@ -0,0 +1,17 @@
+# print('1','2','3','4','5', sep='---')
+#
+# Text = "\tПервая строка с табуляцией\nВторая строка на новой строке"
+# print(Text)
+#
+# Text_2 = 'Это файл "example.txt"'
+# print(Text_2)
+#
+# Text_3 = """'Это файл "example.txt"'"""
+# print(Text_3)
+#
+# a = "Это файл "
+# b = '"example.txt"'
+# print(a+b)
+#
+# d = "'Это файл \"example.txt\"'"
+# print(d)
